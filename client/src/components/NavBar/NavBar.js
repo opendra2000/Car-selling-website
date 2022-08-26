1cda2743c57730a4bacb8f03a16058558d99bffb
opendra2000 <59149614+opendra2000@users.noreply.github.com>
2022-08-27 00:19:15 +0200

    minor change

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -18,7 +18,6 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/sell">Sell</Nav.Link>
             </Nav>
           </Navbar.Collapse>
